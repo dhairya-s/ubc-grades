a9bf1358279ca30820646a650e051cd8eeb07540
dhairya-s <dhairyasingal08@gmail.com>
2023-10-13 11:39:35 -0700

    + commented out AND

--- src/services/collectLogicComp.ts
+++ src/services/collectLogicComp.ts
@@ -36,7 +36,7 @@ export default class CollectLogicComp {
 
 
 		if (key === "AND") {
-			propertiesToAdd = this.handleAndComp(propertiesToLogic);
+			// propertiesToAdd = this.handleAndComp(propertiesToLogic);
 		} else if (key === "OR") {
 			propertiesToAdd = this.handleOrComp(propertiesToLogic);
 		}
