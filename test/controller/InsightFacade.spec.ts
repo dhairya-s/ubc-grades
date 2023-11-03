b5fd3f0355632da809271be0607c90d83eacbc40
anaisrv <anrojas@students.cs.ubc.ca>
2023-11-03 00:50:25 -0700

    Fixing for timeout with foldertest

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -550,7 +550,7 @@ describe("InsightFacade", function () {
 		folderTest<unknown, Promise<InsightResult[]>, PQErrorKind>(
 			"Dynamic InsightFacade PerformQuery Ordered tests",
 			(input) => facade.performQuery(input),
-			"./test/resources/ordered_queries/test_queries",
+			"./test/resources/ordered_queries/RoomTests",
 
 			{
 				assertOnResult: async (actual, expected) => {
